42b8929fba8b96fad9bfb4c088f519fc9fe00ab6
Martina Fumanelli <Martina@iMac-di-Mario.homenet.telecomitalia.it>
2020-06-23 09:36:44 +0200

    prima versione codice

---
+++ parall.py
@@ -0,0 +1,20 @@
+import os                                                                       
+from multiprocessing import Pool                                                
+import telegram                                                                
+                                                                                
+processes = ('perscript.py', 'perscript.py', 'perscript.py', 'perscript.py')                                    
+                                                  
+def notify_ending(message):
+    token = '1116796554:AAEyr52UujgqbXkg1yfCvqn-oF4WMuMKgBw'
+    chat_id = '299870514'
+    bot = telegram.Bot(token=token)
+    bot.sendMessage(chat_id=chat_id, text=message)
+
+def run_process(process):                                                             
+    os.system('python {}'.format(process))                                       
+                                                                                
+                                                                                
+pool = Pool(processes=4)                                                        
+pool.map(run_process, processes)
+
+notify_ending('Il codice ha finito')
