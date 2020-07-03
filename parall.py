d7fe78d69be53518939677411b586594d0365d4b
Martina Fumanelli <Martina@iMac-di-Mario.homenet.telecomitalia.it>
2020-07-03 17:42:50 +0200

    modifica creazione df

--- parall.py
+++ parall.py
@@ -1,20 +1,27 @@
-import os                                                                       
-from multiprocessing import Pool                                                
-import telegram                                                                
-                                                                                
-processes = ('perscript_adattivo.py', 'perscript_adattivo.py', 'perscript_adattivo.py', 'perscript_adattivo.py')                                    
-                                                  
+import os
+from multiprocessing import Pool
+import telegram
+
+processes = (
+    "ricerca_sequenziale.py",
+    "ricerca_sequenziale.py",
+    "ricerca_sequenziale.py",
+    "ricerca_sequenziale.py",
+)
+
+
 def notify_ending(message):
-    token = '1116796554:AAEyr52UujgqbXkg1yfCvqn-oF4WMuMKgBw'
-    chat_id = '299870514'
+    token = "1116796554:AAEyr52UujgqbXkg1yfCvqn-oF4WMuMKgBw"
+    chat_id = "299870514"
     bot = telegram.Bot(token=token)
     bot.sendMessage(chat_id=chat_id, text=message)
 
-def run_process(process):                                                             
-    os.system('python {}'.format(process))                                       
-                                                                                
-                                                                                
-pool = Pool(processes=4)                                                        
+
+def run_process(process):
+    os.system("python {}".format(process))
+
+
+pool = Pool(processes=4)
 pool.map(run_process, processes)
 
-notify_ending('Il codice ha finito')
+notify_ending("Il codice ha finito")
